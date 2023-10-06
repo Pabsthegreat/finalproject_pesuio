26d0551ddc15e712b4965f3fff01b1f96f10b4be
Pabsthegreat <94716164+Pabsthegreat@users.noreply.github.com>
2023-10-06 16:57:20 +0530

    Revert "USE THIS!"
    This reverts commit d23de4194a8a00c620b591d92ec79cc1c5e1a2be.

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,10 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+
 const Header = () => {
     return(
-        <div className="w-screen h-4 sm:h-12 md:h-20 lg:h-16 grid grid-cols-5 place-items-center border-black divide-cyan-100 divide-x-2">
-            <div className="align-middle justify-center flex content-center rounded-full w-1/12 h-1/12 object-contain"><Link href = "/"><Image src = "/Shoes 2.jpeg" width = {500} height = {200} alt = 'pic1'/></Link></div>
+        <div className="w-screen h-4 sm:h-12 md:h-20 lg:h-16 grid grid-cols-5 divide-x-2 ">
+            <div className="border-rounded align-middle justify-center flex border-black">IMG</div>
             <div className="flex col-span-3 justify-center">
                 <input content="text" placeholder="Searchh For anything in Bargain" size={15}></input>
             </div>
